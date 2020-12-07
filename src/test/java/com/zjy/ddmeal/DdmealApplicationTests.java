27747d9be2aca3b110b69a41caca57419c48aff8
Educate <3036664758@qq.com>
2020-12-07 10:25:27 +0800

    第二次测试

--- src/test/java/com/zjy/ddmeal/DdmealApplicationTests.java
+++ src/test/java/com/zjy/ddmeal/DdmealApplicationTests.java
@@ -1,13 +1,16 @@
-package com.zjy.ddmeal;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class DdmealApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.zjy.ddmeal;
+//
+//import org.junit.jupiter.api.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@SpringBootTest
+//@RunWith(SpringRunner.class)
+//class DdmealApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
