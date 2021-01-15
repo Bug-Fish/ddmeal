394919d61bfd1c8031f148fb20f5f5c0b1c10499
张教育 <3036664758@qq.com>
2021-01-15 18:38:34 +0800

    test1

---
+++ src/main/java/com/zjy/ddservice/mapper/DdMenu2Mapper.java
@@ -0,0 +1,8 @@
+package com.zjy.ddservice.mapper;
+
+/**
+ * @author: zhangjiaoyu
+ * @date: 2021/1/15
+ */
+public interface DdMenu2Mapper {
+}
--- src/main/java/com/zjy/ddservice/mapper/DdMenuMapper.java
+++ src/main/java/com/zjy/ddservice/mapper/DdMenuMapper.java
@@ -3,6 +3,8 @@ package com.zjy.ddservice.mapper;
 import com.zjy.ddservice.entity.DdMenu;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
+import java.util.List;
+
 /**
  * <p>
  *  Mapper 接口
@@ -12,5 +14,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  * @since 2020-12-05
  */
 public interface DdMenuMapper extends BaseMapper<DdMenu> {
+
+    List<String> findAll();
 
 }
